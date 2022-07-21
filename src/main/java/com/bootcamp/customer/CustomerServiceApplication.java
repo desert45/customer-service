226128c07e9572bfe0f89448272e7af4c2c19dd5
Antony Fernandez <62619196+desert45@users.noreply.github.com>
2022-07-21 00:49:11 -0500

    commit afc

--- src/main/java/com/bootcamp/customer/CustomerServiceApplication.java
+++ src/main/java/com/bootcamp/customer/CustomerServiceApplication.java
@@ -1,7 +1,11 @@
 package com.bootcamp.customer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -10,4 +14,17 @@ public class CustomerServiceApplication {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+    @Value("${service.name}")
+    private String name;
+
+    @RequestMapping("/name")
+    String getMessage() {
+        return this.name;
+    }
 }
