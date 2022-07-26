a9c01942179ff484b759a4b5973fd2ed7a918972
Antony Fernandez <62619196+desert45@users.noreply.github.com>
2022-07-26 16:31:01 -0500

    commit afc

--- src/main/java/com/bootcamp/customer/CustomerServiceApplication.java
+++ src/main/java/com/bootcamp/customer/CustomerServiceApplication.java
@@ -1,12 +1,10 @@
 package com.bootcamp.customer;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CustomerServiceApplication {
 
@@ -14,17 +12,4 @@ public class CustomerServiceApplication {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
-}
-
-@RefreshScope
-@RestController
-class MessageRestController {
-
-    @Value("${service.name}")
-    private String name;
-
-    @RequestMapping("/name")
-    String getMessage() {
-        return this.name;
-    }
 }
